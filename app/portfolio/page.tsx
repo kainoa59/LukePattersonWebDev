51400a5ea7ea4b40762be50d1b3c6ef905e3c594
kainoa59 <kainoa59@gmail.com>
2025-07-02 14:28:31 -0700

    added cards and titles to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,6 +4,15 @@
 import { useEffect, useState } from "react";
 import PageTransition from "@/components/PageTransition";
 import CoverPageTransition from "@/components/CoverPageTransition";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function PortfolioPage() {
   const [showCover, setShowCover] = useState(false);
@@ -40,7 +49,60 @@ export default function PortfolioPage() {
             transition: "opacity 1.5s cubic-bezier(.77,0,.18,1)",
           }}
         >
-          <h1 className="text-3xl font-bold text-white p-8">Portfolio</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-15 lg:gap-20 p-10 md:p-15 lg:p-20  w-full">
+            <Card>
+              <CardHeader>
+                <CardTitle>C++ Robotics</CardTitle>
+                <CardDescription>first card description</CardDescription>
+                <CardAction>Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>content for card one</p>
+              </CardContent>
+              <CardFooter>
+                <p>footer one</p>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Web Development</CardTitle>
+                <CardDescription>second card description</CardDescription>
+                <CardAction>Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>content for card two</p>
+              </CardContent>
+              <CardFooter>
+                <p>footer two</p>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Virtualization</CardTitle>
+                <CardDescription>third card description</CardDescription>
+                <CardAction>Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>content for card three</p>
+              </CardContent>
+              <CardFooter>
+                <p>footer three</p>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>3D Game Programming</CardTitle>
+                <CardDescription>fourth card description</CardDescription>
+                <CardAction>Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>content for card four</p>
+              </CardContent>
+              <CardFooter>
+                <p>footer four</p>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
         {/* Portfolio content here */}
       </main>
